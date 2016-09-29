7a4cea7eea039e4c0c8664586523bc5e66311476
Hugo Andrés Yupanqui Gómez <Hugo Andrs Yupanqui Gmez>
2016-09-29 15:41:35 -0500

    Myform.cpp implementado

--- Trabajo_Parcial_201602/MyForm.cpp
+++ Trabajo_Parcial_201602/MyForm.cpp
@@ -1,2 +1,10 @@
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ arg) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew Trabajo_Parcial_201602::MyForm());
+}
